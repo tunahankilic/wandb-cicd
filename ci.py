4655840ad4658ae2643329fbe50f0324afe41fee
Tunahan Kilic <kilic.tunahan@gmail.com>
2026-03-05 20:04:05 +0100

    add workflow that runs python script

---
+++ ci.py
@@ -0,0 +1,5 @@
+import wandb
+import os
+
+
+print(f"The version of wandb is: {wandb.__version__}")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from wandb-cicd!")
+
+
+if __name__ == "__main__":
+    main()
